81b08520f60cefc0d7b1341a6f76e9d074aa01db
claudioed <claudioed.oliveira@gmail.com>
2021-04-27 08:11:23 -0300

    logger

--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/rs/zerolog v1.21.0
 	github.com/valyala/fasttemplate v1.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20191105034135-c7e5f84aec59 // indirect
 )
--- main.go
+++ main.go
@@ -3,13 +3,45 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/rs/zerolog"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 )
+
+var log *zerolog.Logger
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	logger := zerolog.New(output).With().Timestamp().Caller().Logger()
+	log = &logger
+}
 
 func main() {
 	e := echo.New()
 	// Middleware
-	e.Use(middleware.Logger())
+	e.Logger.SetOutput(ioutil.Discard)
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			req := c.Request()
+			res := c.Response()
+			start := time.Now()
+			log.Debug().
+				Interface("headers", req.Header).
+				Msg(">>> " + req.Method + " " + req.RequestURI)
+			if err = next(c); err != nil {
+				c.Error(err)
+			}
+			log.Debug().
+				Str("latency", time.Now().Sub(start).String()).
+				Int("status", res.Status).
+				Interface("headers", res.Header()).
+				Msg("<<< " + req.Method + " " + req.RequestURI)
+			return
+		}
+	})
 	e.Use(middleware.Recover())
 
 	e.Static("/static", "assets")
